7b1b264f2f8bfacc5445d3a95987f17b326bf53d
technicalsupport-linux <256074974@protonmail.com>
2026-02-13 16:15:30 -0600

    Create technicalsupport-linux.js

---
+++ technicalsupport-linux.js
@@ -0,0 +1,12 @@
+{
+  "accessNodeUrl": "https://rest-testnet.onflow.org",
+  "discoveryWallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "discoveryAuthnEndpoint": "https://fcl-discovery.onflow.org/api/testnet/authn",
+  "flowNetwork": "testnet",
+  "appDetailTitle": "zbgk firewall app",
+  "appDetailUrl": "https://react.flow.com",
+  "appDetailIcon": "https://avatars.githubusercontent.com/u/62387156?v=4",
+  "appDetailDescription": "Your app description",
+  "computeLimit": 1000,
+  "walletconnectProjectId": "id1438144202"
+}
